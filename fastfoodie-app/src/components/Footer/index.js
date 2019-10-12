11b88bb214a6bd23beac9e9553a31c551423e36e
yashua <yashuab.abraham@gmail.com>
2019-10-11 22:39:01 -0400

    fixed layout issuse with horizontal scroll being needed  because excessive space was being used

--- fastfoodie-app/src/components/Footer/index.js
+++ fastfoodie-app/src/components/Footer/index.js
@@ -1,9 +1,9 @@
 import React from "react"
-
+import "./style.css"
 const Footer = () => {
   return(
     <div>
-      <p>CopyRight 2019</p>
+      <p className="copy-right">CopyRight 2019</p>
     </div>
   )
 }
