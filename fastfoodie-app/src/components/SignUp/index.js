d06baa093d9a8ab365ba2cd999b989514449d2b4
yashua <yashuab.abraham@gmail.com>
2019-11-01 16:21:21 -0400

    change sign up component from dumb to smart

--- fastfoodie-app/src/components/SignUp/index.js
+++ fastfoodie-app/src/components/SignUp/index.js
@@ -1,11 +1,26 @@
 import React from "react"
+import {Component} from "react"
 import "./style.css"
-const SignUp = () => {
-  return(
-    <div>
-      <div className="sign-up-menu">Sign Up Menu</div>
+
+class SignUp extends  Component(){
+    constructor(props){
+      super(props)
+    }
+
+    render(){
+      return(
+        <div>
+       <div className="sign-up-menu">Sign Up Menu</div>
     </div>
-  )
+      )
+    }
 }
+// const SignUp = () => {
+//   return(
+//     <div>
+//       <div className="sign-up-menu">Sign Up Menu</div>
+//     </div>
+//   )
+// }
 
 export default SignUp
