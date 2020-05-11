5f6e83c341299eba0878077a6a2d4f04621eca70
yashua <yashuab.abraham@gmail.com>
2020-05-11 14:42:18 -0400

    created wrapper component

--- fastfoodie-app/src/App.js
+++ fastfoodie-app/src/App.js
@@ -4,16 +4,17 @@ import Header from '../src/components/Header/index'
 import Footer from '../src/components/Footer/index'
 import NavBar from '../src/components/Navbar/index'
 import ResultPage from './components/Pages/ResultsPage';
+import Wrapper from './components/Wrapper';
 
 function App() {
   return (
-    <div>
+    <Wrapper>
       <Header />
       <NavBar />
       <Home />
       <ResultPage />
       <Footer />
-    </div>
+    </Wrapper>
   );
 }
 
--- fastfoodie-app/src/components/Footer/index.js
+++ fastfoodie-app/src/components/Footer/index.js
@@ -2,7 +2,7 @@ import React from "react"
 import "./style.css"
 const Footer = () => {
   return(
-    <div>
+    <div className="footer">
       <p className="copy-right">CopyRight 2019</p>
     </div>
   )
