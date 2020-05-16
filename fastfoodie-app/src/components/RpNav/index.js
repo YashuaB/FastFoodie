3c672d563fbd9418d81077a1f8bc062d79410637
yashua <yashuab.abraham@gmail.com>
2020-05-16 05:26:12 -0400

    created 2nd nav component

---
+++ fastfoodie-app/src/components/RpNav/index.js
@@ -0,0 +1,18 @@
+import React from "react"
+import "./style.css"
+
+const RpNav = () =>{
+  return(
+    <div>
+    <div className="nav">
+      <ul>
+      <li><a href="#home">Home</a></li>
+      <li><a href="#contact">Contact</a></li>
+      <li><a href="#about">About</a></li>
+      </ul>
+    </div>
+    </div>
+  )
+}
+
+export default RpNav
