7bea5adea4ff61b4bb2363f5c8b173e0f7f38396
yashua <yashuab.abraham@gmail.com>
2020-05-26 00:36:17 -0400

    fixed text in result card, repositioned elements within result card

--- fastfoodie-app/src/components/ResultCard/index.js
+++ fastfoodie-app/src/components/ResultCard/index.js
@@ -6,9 +6,9 @@ import "./style.css"
 const Card = () =>{
   return (
     <div className="result-container">
-      <h2 className="rs-title"></h2>
+      <p className="rs-title"></p>
       <div className="result-body">
-        <img alt="restaurant" src="https://via.placeholder.com/150x100.png"/>
+        <img className="rs-image"alt="restaurant" src="https://via.placeholder.com/230x240.png"/>
         <div className="result-text-body">
         <h2 className="rs-restaurant-name">Restaurant Name</h2>
         <p className="rs-restaurant-info">Lorem ipsum dolor sit amet consectetur adipiscing elit.</p>
