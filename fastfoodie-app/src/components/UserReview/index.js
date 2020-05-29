75336e6259a81238f72fbf0339ca5879c794b3a2
yashua <yashuab.abraham@gmail.com>
2020-05-29 01:23:28 -0400

    repositioned user review card

--- fastfoodie-app/src/components/UserReview/index.js
+++ fastfoodie-app/src/components/UserReview/index.js
@@ -14,7 +14,12 @@ class UserReview extends Component {
     return (
       <div className="us-review-container">
         <div className="us-review-body">
-        <textarea type="text" placeholder="your review"></textarea>
+        <div className="user-details">
+          <img className="user-details-img" src="https://via.placeholder.com/60x60.png"/>
+          <p className="user-detail-name">Morty S.</p>
+        </div>
+        <p className="sub-title">Write your review</p>
+        <textarea className="user-text" type="text" placeholder="your review"></textarea>
         </div>
       </div>
     )
