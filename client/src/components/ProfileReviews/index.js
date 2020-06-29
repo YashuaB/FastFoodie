54d40db1d76524d983587cdba9b507e8200e18bf
yashuab <yashuab.abraham@gmail.com>
2020-06-29 02:27:00 -0400

    create profile review titles and added css

--- client/src/components/ProfileReviews/index.js
+++ client/src/components/ProfileReviews/index.js
@@ -1,12 +1,23 @@
 import React from "react"
 import {Component} from "react"
 import "./style.css"
+import reviewedData from "./reviewed.json"
 
+const Reviewed = (props) => {
+    return (
+      <div className="pr-reviews-detail">
+        <div className="pr-res-name">{props.resName}</div>
+        <div className="pr-review-date">Reviewed on: {props.date}</div>
+        <div className="pr-res-rating">You rated: {props.resRating}</div>
+        <a href={props.resLink}><img className="pr-res-image" alt={props.resAlt} src={props.resImage}/></a>
+      </div>
+    )
+}
 
 class ProfileReviews extends Component {
   
   state = {
-
+    reviewedData
   }
 
   render () {
@@ -15,7 +26,17 @@ class ProfileReviews extends Component {
         <div className="pr-reviews-container">
           <div className="pr-reviews-body">
             <h2 className="pr-title">Your Reviews</h2>
-              <span className="underline"></span>
+              <div className="underline"></div>
+              {this.state.reviewedData.map( data => (
+                <Reviewed
+                resName={data.restaurantName}
+                date={data.date}
+                resRating={data.rating}
+                resLink={data.restaurantUrl}
+                resAlt={data.restaurantName}
+                resImage={data.restaurantImage}
+                />
+              ))}
           </div>
         </div>
       </div>
